d9c8e4b6c352a51a1dcb50af8064d586d4345462
Philipp Heckel <pheckel@datto.com>
2019-10-25 07:49:32 -0400

    Log and mod file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module heckel.io/swift/packer
+
+go 1.13
---
+++ log.go
@@ -0,0 +1,49 @@
+package packer
+
+import (
+	"fmt"
+	"log"
+	"strings"
+)
+
+var (
+	Debug = false // Toggle with -debug CLI flag! // TODO fix this with debug/log levels
+	Quiet = false // TODO fix this with log levels
+
+	statusLastLength = 0
+)
+
+func init() {
+	log.SetFlags(log.Lmicroseconds)
+}
+
+func debugf(format string, args ...interface{}) {
+	if Quiet {
+		return
+	}
+
+	if Debug {
+		log.Printf(format, args...)
+	}
+}
+
+func statusf(format string, args ...interface{}) {
+	if Quiet {
+		return
+	}
+
+	if Debug {
+		fmt.Printf(format + "\n", args...)
+	} else {
+		status := fmt.Sprintf(format, args...)
+		statusNewLength := len(status)
+
+		if statusNewLength < statusLastLength {
+			fmt.Printf("\r%s\r%s", strings.Repeat(" ", statusLastLength), status)
+		} else {
+			fmt.Printf("\r%s", status)
+		}
+
+		statusLastLength = statusNewLength
+	}
+}
